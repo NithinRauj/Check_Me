a173027744c7e7c5e2f677476fd1e39b4c96f5a9
Nithin Rauj <nithindrake@gmail.com>
2019-10-14 19:17:59 +0530

    Created basic UI of app

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -5,6 +5,70 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              top: 60.0,
+              left: 20.0,
+              right: 20.0,
+              bottom: 20.0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.check_box,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      'Check Me',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  '17 tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 400.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
