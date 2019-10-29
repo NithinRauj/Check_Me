60c8f18aa0a4c97f132d32a29194ab71db746060
Nithin Rauj <nithindrake@gmail.com>
2019-10-29 19:31:41 +0530

    Created a builder for tasks

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -1,45 +1,26 @@
 import 'package:flutter/material.dart';
 
-class ListTileWidget extends StatefulWidget {
-  @override
-  _ListTileWidgetState createState() => _ListTileWidgetState();
-}
+class ListTileWidget extends StatelessWidget {
+  final bool isChecked;
+  final String taskName;
+  final Function checkboxCallback;
 
-class _ListTileWidgetState extends State<ListTileWidget> {
-  bool isChecked = false;
-
-  void checkboxCallback(bool currentState) {
-    setState(() {
-      isChecked = currentState;
-    });
-  }
+  ListTileWidget({this.taskName, this.isChecked, this.checkboxCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'Buy Eggs',
+        taskName,
         style: TextStyle(
           decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: TaskCheckboxWidget(isChecked, checkboxCallback),
-    );
-  }
-}
-
-class TaskCheckboxWidget extends StatelessWidget {
-  final bool checkboxState;
-  final Function toggleCheckboxCallback;
-
-  TaskCheckboxWidget(this.checkboxState, this.toggleCheckboxCallback);
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      activeColor: Colors.teal,
-      value: checkboxState,
-      onChanged: toggleCheckboxCallback,
+      trailing: Checkbox(
+        activeColor: Colors.teal,
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
     );
   }
 }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'list_tile.dart';
+import 'package:check_me_app/models/task.dart';
 
-class TasksListWidget extends StatelessWidget {
+class TasksListWidget extends StatefulWidget {
+  @override
+  _TasksListWidgetState createState() => _TasksListWidgetState();
+}
+
+class _TasksListWidgetState extends State<TasksListWidget> {
+  List<Task> tasks = [
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy bread')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTileWidget(),
-        ListTileWidget(),
-        ListTileWidget(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTileWidget(
+          taskName: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkboxCallback: (currentState) {
+            setState(() {
+              tasks[index].toggleIsDone();
+            });
+          },
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
