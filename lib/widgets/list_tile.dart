8feb91227271c36569cd306c8199d29683657636
Nithin Rauj <nithindrake@gmail.com>
2019-10-29 18:19:59 +0530

    Implemented functionality for checkboxes of tasks

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ListTileWidget extends StatelessWidget {
+class ListTileWidget extends StatefulWidget {
+  @override
+  _ListTileWidgetState createState() => _ListTileWidgetState();
+}
+
+class _ListTileWidgetState extends State<ListTileWidget> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool currentState) {
+    setState(() {
+      isChecked = currentState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Buy Eggs'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'Buy Eggs',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckboxWidget(isChecked, checkboxCallback),
+    );
+  }
+}
+
+class TaskCheckboxWidget extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxCallback;
+
+  TaskCheckboxWidget(this.checkboxState, this.toggleCheckboxCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.teal,
+      value: checkboxState,
+      onChanged: toggleCheckboxCallback,
     );
   }
 }
